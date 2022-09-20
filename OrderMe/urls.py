7b56952c4ae4d5132e7c6e732909647f8322509f
hamza <mhamzabaig1234@gmail.com>
2022-09-21 02:20:32 +0500

    fixing bugs in restraunt app

--- OrderMe/urls.py
+++ OrderMe/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from . import restraunts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- restraunts/urls.py
+++ restraunts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'restraunt'
 
 urlpatterns = [
-    path('', views.restrautntintro),
+    path('', views.restraunt_intro),
     
 ]

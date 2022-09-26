8eaa56c3527900486db44741a47518358fbaaf46
hamza <mhamzabaig1234@gmail.com>
2022-09-26 17:25:08 +0500

    restraunt model added

--- restraunts/models.py
+++ restraunts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class restraunt(models.Model):
+    # Id will be defaulted primary key
+    name = models.TextField(max_length=50)
+    reg_id = models.TextField(max_length=50)
+    contact_no = models.IntegerField
+    email_id = models.TextField
+

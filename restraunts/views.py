f75b95f3dd9325ac25767db0d5cd7f8b7863e91b
hamza <mhamzabaig1234@gmail.com>
2022-09-27 05:40:56 +0500

    Naming Conventions added

--- restraunts/views.py
+++ restraunts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def restraunt_intro(request):
-    return render(request,'restraunts/home.html')
+    return render(request,'restraunts/res_welcome.html')

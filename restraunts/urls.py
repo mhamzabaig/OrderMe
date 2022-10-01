4585ea1ce1355682329f095b09f77ceb38d59990
hamza <mhamzabaig1234@gmail.com>
2022-10-01 08:35:22 +0500

    Sign up of restraunts added

--- restraunts/urls.py
+++ restraunts/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 
-app_name = 'restraunt'
+app_name = 'restraunts'
 
 urlpatterns = [
     path('', views.restraunt_intro,name = 'res_intro'),

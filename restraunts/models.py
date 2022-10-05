86d97d572989a4caef5062882a6a3bbe3a754f72
hamza <mhamzabaig1234@gmail.com>
2022-10-05 16:08:56 +0500

    restraunts model updated

--- restraunts/models.py
+++ restraunts/models.py
@@ -6,6 +6,6 @@ class restraunt(models.Model):
     # Id will be defaulted primary key
     name = models.TextField(max_length=50)
     reg_id = models.TextField(max_length=50)
-    contact_no = models.IntegerField
-    email_id = models.TextField
+    contact_no = models.IntegerField(blank=True,null=True)
+    email_id = models.TextField(max_length=50,blank=False,null=True)
 

11745c22452ab50ce9057d98591c9c71ddb4889c
hamza <mhamzabaig1234@gmail.com>
2022-10-09 11:27:46 +0500

    Log out not working properly

--- restraunts/views.py
+++ restraunts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+@login_required(login_url="res_accounts/login/")
 def restraunt_intro(request):
     return render(request,'restraunts/baselayout.html')

bc0040009f9adc6b5a13ad6fe665e947e65959d7
hamza <mhamzabaig1234@gmail.com>
2022-10-19 11:52:14 +0500

    Creating restraunt forms

--- restraunts/views.py
+++ restraunts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .models import restraunt
+from .models import User
 # Create your views here.
 
 @login_required(login_url="res_accounts/login/")
 def restraunt_intro(request):
-    return render(request,'restraunts/baselayout.html')
+    username = request.user.username
+    user_id = User.objects.get(username = username)
+    Restraunt = restraunt.objects.get(res_owner = user_id)
+    print(Restraunt.name)    
+    return render(request,'restraunts/baselayout.html',{'restraunt':Restraunt})
 
 def edit_profile(request):
     return HttpResponse('Edit your Profile')
